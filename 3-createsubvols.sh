10b3725d95311235058a6e6d591a547962186bc8
vmath3us <vmatheusborges@gmail.com>
2022-07-04 14:53:32 -0400

    create and set permissions btrfs subvol

--- 3-createsubvols.sh
+++ 3-createsubvols.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 root_dir=$(mktemp -d -p /tmp)
-default_root="@base_system"
-block_device_root=$(grub-probe / --target=device) #######################or insert here, into quotes
-mount $block_device_root $default_root -o "subvolid="5 &&
-btrfs su cr $default_root/@home &&\
-btrfs su cr $default_root/@flatpak &&\
-btrfs su cr $default_root/@Docker &&\
-btrfs su cr $default_root/@Podman &&\
-btrfs su cr $default_root/@Documents &&\
-btrfs su cr $default_root/@Images &&\
-btrfs su cr $default_root/@Musics &&\
-btrfs su cr $default_root/@Videos &&\
-btrfs su cr $default_root/@Downloads &&\
-read -p "name:group owner home folders: " setperm
-chown -R $setperm $default_root/* &&\
-btrfs su cr $default_root/@cache &&\
-btrfs su cr $default_root/@log &&\
-btrfs filesystem sync $default_root &&\
-exit 0
+block_device_root="/dev/sda3" ###################### insert here, into quotes
+function create(){
+mount $block_device_root -o "subvolid="5 $root_dir &&
+btrfs su cr $root_dir/@cache
+btrfs su cr $root_dir/@log
+btrfs su cr $root_dir/@home
+read -p "owner:group home dirs: " perm
+for i in @userflatpak @Docker @Podman @Documents @Pictures @Music @Videos @Downloads; do
+	btrfs su cr $root_dir/$i
+	chown -R $perm $root_dir/$i
+done
+}
+create
