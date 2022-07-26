0508ec4a4414c30adc17c99fb7761c5b013216c3
Victor Matheus <vmatheusborges@gmail.com>
2022-07-26 09:54:28 -0400

    ArchGnome Workstation list explicit

--- generate-pacman-list.sh
+++ generate-pacman-list.sh
@@ -2,12 +2,12 @@
 function no-devel(){
 execute=$(pacman -Qqe | grep -Ev "$(pacman -Qqm)" | grep -Ev "$(pacman -Qg base-devel | cut -d " " -f2)" > 1.1-packagelist-not-devel.pacman) &&
     git commit -am "ArchGnome Workstation list explicit, no base-devel" &&
-    git pull
+    git push
 }
 function list-package(){
     pacman -Qqe | grep -Ev "$(pacman -Qqm)" > 1-packagelist.pacman
     git commit -am "ArchGnome Workstation list explicit" &&
-    git pull &&
+    git push &&
     no-devel
 }
 list-package
