15385b4f6d56e93407b3084100598fc1df869c5f
Victor Matheus <vmatheusborges@gmail.com>
2022-08-08 10:07:30 -0400

    generate flatpak list

--- generate-flatpak-list.sh
+++ generate-flatpak-list.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-flatpak list --app --columns=remote,application > flatpak.list
+flatpak list --app --columns=origin,application > flatpak.list &&
+    git commit -am "flatpak list, including repo" &&
+    git push
