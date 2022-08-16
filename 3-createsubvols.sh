888b88270a151c9190f40e78d9a70a466227c61c
Victor Matheus <vmatheusborges@gmail.com>
2022-08-16 11:38:56 -0400

    change home subvol perms and mountpoint

--- 3-createsubvols.sh
+++ 3-createsubvols.sh
@@ -5,9 +5,8 @@ function create(){
 mount $block_device_root -o "subvolid="5 $root_dir &&
 btrfs su cr $root_dir/@cache
 btrfs su cr $root_dir/@log
-btrfs su cr $root_dir/@home
 read -p "owner:group home dirs: " perm
-for i in @flatpak @Docker @Podman @fonts @Documents @Pictures @Music @Videos @Downloads; do
+for i in @flatpak @Docker @Podman @fonts @home @Documents @Pictures @Music @Videos @Downloads; do
 	btrfs su cr $root_dir/$i
 	chown -R $perm $root_dir/$i
 done
