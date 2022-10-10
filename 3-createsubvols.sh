5eda13bc300191ef2261069fb6787252d8a05ffb
vmath3us <vmatheusborges@gmail.com>
2022-10-10 14:57:09 -0400

    create/mount vm subvol, set nocow

--- 3-createsubvols.sh
+++ 3-createsubvols.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 root_dir=$(mktemp -d -p /tmp)
-block_device_root="name_block_device_here" ###################### insert here, into quotes
+block_device_root=$(grub-probe / --target=device)
 function create(){
 mount $block_device_root -o "subvolid="5 $root_dir &&
 btrfs su cr $root_dir/@cache
 btrfs su cr $root_dir/@log
 read -p "owner:group home dirs: " perm
-for i in @flatpak @Docker @Podman @fonts @home @Documents @Pictures @Music @Videos @Downloads; do
+for i in @flatpak @Docker @Podman @fonts @home @Documents @Pictures @Music @Videos @Downloads @VMs; do
 	btrfs su cr $root_dir/$i
 	chown -R $perm $root_dir/$i
 done
+chattr +C $root_dir/@VMs
 }
 create
