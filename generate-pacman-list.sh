29bf33bfe74a9ec724c2f238b95a295ddd22623d
vmath3us <vmatheusborges@gmail.com>
2022-11-22 14:12:15 -0400

    change commands to generate pacman list - new paramater to exclude AUR packages

--- generate-pacman-list.sh
+++ generate-pacman-list.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 function no-devel(){
-execute=$(pacman -Qqe | grep -Ev "$(pacman -Qqm)" | grep -Ev "$(pacman -Qg base-devel | cut -d " " -f2)" > 1.1-packagelist-not-devel.pacman) &&
+execute=$(pacman -Qqen | grep -Ev "$(pacman -Qg base-devel | cut -d " " -f2)" > 1.1-packagelist-not-devel.pacman) &&
     git commit -am "ArchGnome Workstation list explicit, no base-devel" &&
     git push
 }
 function list-package(){
-    pacman -Qqe | grep -Ev "$(pacman -Qqm)" > 1-packagelist.pacman
+    pacman -Qqen > 1-packagelist.pacman
     git commit -am "ArchGnome Workstation list explicit" &&
     git push &&
     no-devel
 }
 list-package
-
-
