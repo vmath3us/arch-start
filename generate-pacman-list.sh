3c5ad7f1ce0b4015018e1dae18a7550d2ca646cf
vmath3us <vmatheusborges@gmail.com>
2023-01-23 09:10:12 -0400

    internal commit script

--- generate-pacman-list.sh
+++ generate-pacman-list.sh
@@ -2,12 +2,12 @@
 function no-devel(){
 execute=$(pacman -Qqen | grep -Ev "$(pacman -Qg base-devel | cut -d " " -f2)" > 1.1-packagelist-not-devel.pacman) &&
     git commit -am "ArchGnome Workstation list explicit, no base-devel" &&
-    git push
+    ./commit
 }
 function list-package(){
     pacman -Qqen > 1-packagelist.pacman
     git commit -am "ArchGnome Workstation list explicit" &&
-    git push &&
+    ./commit &&
     no-devel
 }
 list-package
